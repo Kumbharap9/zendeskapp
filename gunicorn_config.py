d4eb1bf8bf56d5832c7171119a473ad2b2cd0d20
Kumbharap9 <85327507+Kumbharap9@users.noreply.github.com>
2025-07-26 11:26:19 +0530

    Create gunicorn_config.py

---
+++ gunicorn_config.py
@@ -0,0 +1,18 @@
+# gunicorn_config.py
+
+
+workers = 4
+
+bind = "0.0.0.0:5000"
+
+loglevel = "info"
+
+accesslog = "-"
+
+errorlog = "-"
+
+timeout = 30
+
+max_requests = 1000
+
+max_requests_jitter = 50
