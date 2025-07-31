3996309a01ed97d002633f4eb09c0c3cfd9bf389
Kumbharap9 <85327507+Kumbharap9@users.noreply.github.com>
2025-07-31 09:50:19 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,17 +1,14 @@
 # config.py
 import os
 
-ZENDESK_SUBDOMAIN = os.environ.get('ZENDESK_SUBDOMAIN', 'abhishek-56536')
-ZENDESK_EMAIL = os.environ.get('ZENDESK_EMAIL', 'kumbharap9@gmail.com')
+ZENDESK_SUBDOMAIN = os.environ.get('ZENDESK_SUBDOMAIN', 'test-56536')
+ZENDESK_EMAIL = os.environ.get('ZENDESK_EMAIL', 'test@gmail.com')
 ZENDESK_API_TOKEN = os.environ.get('ZENDESK_API_TOKEN', 'iLobR7EmwQ9uZbiKEWlzSKNBmDW9ZwiAAN4pViGW')
 
-HARDCODED_REQUESTER_EMAIL = 'kumbharap9@gmail.com'
-HARDCODED_ASSIGNEE_EMAIL = 'kumbharap9@gmail.com'
+#HARDCODED_REQUESTER_EMAIL = 'test@gmail.com'
+#HARDCODED_ASSIGNEE_EMAIL = 'test@gmail.com'
+HARDCODED_REQUESTER_EMAIL = os.environ.get('ZENDESK_REQUESTER_EMAIL', 'test@gmail.com')
+HARDCODED_ASSIGNEE_EMAIL = os.environ.get('ZENDESK_ASSIGNEE_EMAIL', 'test@gmail.com')
 
-
-#
 # Use a key generated with python -c "import os; import binascii; print(binascii.hexlify(os.urandom(24)).decode())"
 SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', '0a7a2947005a75d19d69e19b34ff8f29502c8192333e617')
-
-
-
